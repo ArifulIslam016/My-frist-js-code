2efaba0c8c3a343496f0a94ffb1d47ae31ab4d8e
Ariful <arifulq234@gmail.com>
2025-07-28 00:13:02 +0600

    Exploring conditional js

---
+++ conditional.js
@@ -0,0 +1,11 @@
+const applePrice =320;
+const quality= "Ripe";
+
+// if(applePrice<310 || quality=="Ripe"){
+//     console.log("Give me one kg")
+// }
+// else{console.log("Its a loss project")}
+if(applePrice<310 & quality=="Ripe"){
+    console.log("Give me one kg")
+}
+else{console.log("Its a loss project")}
